20b6c225fd003edd6aa3a0eb398133911343ff0a
teten777 <teten.nugraha18@gmail.com>
2019-05-27 05:21:24 +0700

    update CORS bean

---
+++ src/main/java/com/twitter/api/twitter_api/util/GlobalCorsConfiguration.java
@@ -0,0 +1,25 @@
+package com.twitter.api.twitter_api.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class GlobalCorsConfiguration {
+
+    public GlobalCorsConfiguration() {
+        super();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("HEAD","GET","PUT","POST","DELETE","PATCH");
+            }
+        };
+    }
+}
