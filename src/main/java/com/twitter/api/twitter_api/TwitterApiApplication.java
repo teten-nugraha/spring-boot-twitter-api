6683a559ef254441bdb96fb7726a572eaa428464
teten777 <teten.nugraha18@gmail.com>
2019-05-27 05:16:10 +0700

    first

---
+++ src/main/java/com/twitter/api/twitter_api/TwitterApiApplication.java
@@ -0,0 +1,13 @@
+package com.twitter.api.twitter_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterApiApplication.class, args);
+	}
+
+}
